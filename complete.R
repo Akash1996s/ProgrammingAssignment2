692325af46255bf6cdcb2cdbd9d3bdeaedcf3997
Akash1996s <51374209+Akash1996s@users.noreply.github.com>
2020-02-08 14:21:57 +0530

    Delete complete.R

--- complete.R
+++
@@ -1,9 +0,0 @@
-complete <- function(directory, id = 1:332){
-  filel <- list.files(path = directory, pattern = ".csv", full.names = TRUE)
-  nobs <- numeric()
-  for (i in id) {
-    dat <- read.csv(filel[i])
-    nobs <- c(nobs, sum(complete.cases(dat)))
-      }
-  data.frame(id, nobs)
-}
